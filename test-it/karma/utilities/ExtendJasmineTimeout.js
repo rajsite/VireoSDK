6a6cc9aac40f7fa3fdf124a85249ad22d5763046
rajsite <rajsite@users.noreply.github.com>
2017-05-17 09:31:08 -0500

    Increase timeout and add Edge

--- test-it/karma/utilities/ExtendJasmineTimeout.js
+++ test-it/karma/utilities/ExtendJasmineTimeout.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
-    // The PID test takes between 4-6 seconds on PhantomJS so change timeout from default of 5000 to 10000
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    // The PID test takes between 4-6 seconds on PhantomJS
+    // The PI calc tests on IE11 on Sauce Labs take 10-12 seconds
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 }());
