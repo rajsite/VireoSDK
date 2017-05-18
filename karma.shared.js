c9678a8f5410a090631e472a9e855f710cb166b4
Milan Raj <rajsite@users.noreply.github.com>
2017-05-17 23:26:28 -0500

    Enable progress reporter to see what test is causing Edge to timeout

--- karma.shared.js
+++ karma.shared.js
@@ -55,7 +55,7 @@
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dots', 'kjhtml'],
+        reporters: ['progress', 'kjhtml'],
 
         // web server port
         port: 9876,
