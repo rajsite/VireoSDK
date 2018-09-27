37721e00025d7f3f5a8be0d01471a21f3e058c93
rajsite <rajsite@users.noreply.github.com>
2018-09-27 15:23:06 -0500

    Testing modules using unpkg

--- test-it/ManualLoaderTests/loadermodule.js
+++ test-it/ManualLoaderTests/loadermodule.js
@@ -1,10 +1,12 @@
-import vireoHelpers from '../../source/core/vireo.loader.wasm32-unknown-emscripten.release.js';
+import vireoHelpers from 'https://www.unpkg.com/vireo?module';
 
 
 (async function () {
-    var vireo = await vireoHelpers.createInstance({
-        wasmUrl: '../../dist/wasm32-unknown-emscripten/release/vireo.core.wasm'
-    });
+    var vireo = await vireoHelpers.createInstance(
+        {
+            wasmUrl: 'https://www.unpkg.com/vireo/dist/wasm32-unknown-emscripten/release/vireo.core.wasm'
+        }
+    );
 
     var viaCode = document.getElementById('viacode').textContent;
     vireo.eggShell.loadVia(viaCode);
