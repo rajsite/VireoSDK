4789d3962bad2f8e2605cf067e3bae41b504136e
Milan Raj <rajsite@users.noreply.github.com>
2024-04-25 01:26:04 -0500

    Update test.js

--- test-it/test.js
+++ test-it/test.js
@@ -7,8 +7,9 @@
     // Node Test runner for *.via files against Vireo Targets
     // Config file: testList.json
 
-    // Disable fetch in node 20
+    // Disable new APIs in node 20
     global.fetch = undefined;
+    global.Blob = undefined;
 
     require('colors');
     var fs = require('fs'),
